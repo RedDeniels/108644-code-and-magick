4d9724ade69fc27e0501633e288c206b9a7ce907
Dan <dendybox@yandex.ru>
2018-11-21 02:21:02 +0700

    Добавляет функцию, для проверки соответствия имен и рекордов

--- js/stat.js
+++ js/stat.js
@@ -21,7 +21,7 @@ var CHART_IDENT = 50;
 var BAR_WIDTH = 40;
 var BAR_COLOR = 'hsl(240, r, 50%)';
 var BAR_USER_COLOR = 'rgba(255, 0, 0, 1)';
-var BAR_TEXT_IDENT = 5;
+var BAR_TEXT_IDENT = 8;
 
 var getChartY = function (cloudY, textIdent, amountLines, line) {
   return cloudY + textIdent + amountLines * line;
@@ -41,7 +41,7 @@ var renderCloudText = function (ctx, x, y, textLines, font, size, color, lineHei
   }
 };
 
-var roundElements = function (arr) {
+var roundingElements = function (arr) {
   for (var i = 0; i < arr.length; i++) {
     arr[i] = Math.round(arr[i]);
   }
@@ -97,19 +97,37 @@ var renderBarTime = function (ctx, x, y, time, lineHeight, font, size, color) {
   ctx.fillText(time, x, y - lineHeight);
 };
 
+var checkRecord = function (names, times) {
+  if (times.length > names.length) {
+    for (var i = names.length; i < times.length; i++) {
+      names[i] = 'unknown';
+    }
+  } else if (names.length > times.length) {
+    for (var t = times.length; t < names.length; t++) {
+      times[t] = 0;
+    }
+  }
+};
+
+var calculationBar = function (cloud, ident, bar) {
+  return Math.floor((cloud - ident) / (bar + ident));
+};
+
 window.renderStatistics = function (ctx, names, times) {
-  var ChartY = getChartY(CLOUD_Y, CLOUD_TEXT_IDENT, CLOUD_TEXT_LINES.length + 1, LINE_HEIGHT);
+  var chartY = getChartY(CLOUD_Y, CLOUD_TEXT_IDENT, CLOUD_TEXT_LINES.length + 1, LINE_HEIGHT);
+  var amountBar = calculationBar(CLOUD_WIDTH, CHART_IDENT, BAR_WIDTH);
+  checkRecord(names, times);
   renderCloud(ctx, CLOUD_X + CLOUD_SHADOW_IDENT, CLOUD_Y + CLOUD_SHADOW_IDENT, CLOUD_SHADOW_COLOR);
   renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
   renderCloudText(ctx, CLOUD_X + CLOUD_TEXT_IDENT, CLOUD_Y + CLOUD_TEXT_IDENT, CLOUD_TEXT_LINES, FONT, FONT_SIZE, FONT_COLOR, LINE_HEIGHT);
-  roundElements(times);
+  roundingElements(times);
 
-  for (var i = 0; i < times.length; i++) {
+  for (var i = 0; i < amountBar; i++) {
     var barX = getBarX(CLOUD_X + CHART_IDENT, CHART_IDENT, i, BAR_WIDTH);
     var barHeight = getBarHeight(getMaxElement(times), CHART_HEIGHT, times[i]);
-    var barY = getBarY(ChartY, CHART_HEIGHT, barHeight);
+    var barY = getBarY(chartY, CHART_HEIGHT, barHeight);
     renderBar(ctx, barX, barY, names[i], BAR_WIDTH, barHeight, BAR_COLOR, BAR_USER_COLOR);
-    renderBarName(ctx, barX, names[i], ChartY, CHART_HEIGHT, FONT, FONT_SIZE, FONT_COLOR, BAR_TEXT_IDENT);
+    renderBarName(ctx, barX, names[i], chartY, CHART_HEIGHT, FONT, FONT_SIZE, FONT_COLOR, BAR_TEXT_IDENT);
     renderBarTime(ctx, barX, barY, times[i], LINE_HEIGHT, FONT, FONT_SIZE, FONT_COLOR);
   }
 };
