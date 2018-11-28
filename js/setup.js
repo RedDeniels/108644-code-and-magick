4993ad116429a63c8805ce55c180c0e1dbb57c07
Dan <dendybox@yandex.ru>
2018-11-28 17:49:46 +0700

    Добавляет функцию генерации рандомного числа

---
+++ js/setup.js
@@ -0,0 +1,26 @@
+'use strict';
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристофор', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAME = ['да Мария', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var AMOUNT_WIZARDS = 4;
+var wizardsList = [];
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+var randomCount = function (max) {
+  return Math.floor(Math.random() * max);
+};
+
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < AMOUNT_WIZARDS; i++) {
+  fragment.appendChild(renderWizard(i));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
